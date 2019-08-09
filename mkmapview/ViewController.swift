0bb2ea2a72ca44a92eb46c7f4e8ef0d7726c5cb3
cmilhench <cmilhench@gmail.com>
2019-08-09 12:32:00 +0100

    Add example MapKit implementation

--- mkmapview/ViewController.swift
+++ mkmapview/ViewController.swift
@@ -6,15 +6,104 @@
 //  Copyright © 2019 Colin Milhench. All rights reserved.
 //
 
+import MapKit
 import UIKit
 
 class ViewController: UIViewController {
+    let locationManager = CLLocationManager()
+
+    @IBOutlet var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // https://wiki.openstreetmap.org/wiki/Tile_servers
+        let overlayURL = "https://cartodb-basemaps-a.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
+
+        let overlayPath = overlayURL
+        let overlay = MKTileOverlay(urlTemplate: overlayPath)
+        overlay.canReplaceMapContent = true
+        self.mapView.addOverlay(overlay)
+
+        let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olympics")
+        let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.")
+        let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called the City of Light")
+        let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.")
+        let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.")
+
+        mapView.addAnnotations([london, oslo, paris, rome, washington])
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        checkLocationAuthorizationStatus()
+
+        let loc = CLLocation(latitude: 51.507222, longitude: -0.1275)
+        let radius: CLLocationDistance = 5000
+        let region = MKCoordinateRegion(center: loc.coordinate, latitudinalMeters: radius, longitudinalMeters: radius)
+        mapView.setRegion(region, animated: true)
     }
 
+    func checkLocationAuthorizationStatus() {
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
 
+}
+
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let annotation = annotation as? Capital else { return nil }
+
+        let identifier = "Capital"
+
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView?.canShowCallout = true
+            annotationView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        } else {
+            annotationView?.annotation = annotation
+        }
+
+        annotationView?.image = UIImage(named:annotation.imageName)!
+
+        return annotationView
+    }
+
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        guard let capital = view.annotation as? Capital else { return }
+
+        let placeName = capital.title
+        let placeInfo = capital.info
+
+        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        guard let tileOverlay = overlay as? MKTileOverlay else {
+            return MKOverlayRenderer(overlay: overlay)
+        }
+        return MKTileOverlayRenderer(tileOverlay: tileOverlay)
+    }
+}
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    var imageName: String! = "flag"
+
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
 }
 
